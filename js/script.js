e5854eb874d76ef3cc0ec82037c3224c24e342ef
semmr <6033181@mborijnland.nl>
2022-10-31 12:20:13 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function lightblue() {
+	document.body.style.backgroundColor="lightblue"
+}
+function lightred() {
+    document.body.style.backgroundColor="lightcoral"
+}
+function lightyellow(){
+    document.body.style.backgroundColor="lightyellow"
+}
+function lightgray(){
+    document.body.style.backgroundColor="lightgray"
+}
+function white(){
+    document.body.style.backgroundColor="white"
+}
---
+++ php/index.php
@@ -0,0 +1,26 @@
+<?php
+$DataReceived = ( !empty( $_REQUEST['submitted'])) ? $_REQUEST['submitted'] : ' ';
+if (!empty($DataReceived))
+{
+    $name = trim($_REQUEST['name']);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+       body{
+        text-align: center;
+       } 
+    </style>
+</head>
+<body>
+    <form>
+        <input type="text" name="name" value="<?php echo $_POST['name']?>">
+    </form>
+</body>
+</html>
